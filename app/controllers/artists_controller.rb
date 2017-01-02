47291f924ee37a9344bddc8db1825abcf7ef82e6
BeejLuig <bjcantlupe@gmail.com>
2017-01-02 15:37:09 -0500

    add artists_controller routes

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,4 +2,14 @@ class ArtistsController < ApplicationController
   get '/artists' do
     erb :'/artists/index'
   end
+
+  get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'/artists/show'
+  end
+
+  get '/artists/:slug/edit' do
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'/artists/edit'
+  end
 end
