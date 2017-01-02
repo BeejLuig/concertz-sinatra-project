6e833c7fbcc9418d89d84e9b1d7cee3971190c9f
BeejLuig <bjcantlupe@gmail.com>
2017-01-02 16:16:33 -0500

    Add User routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,14 @@ class UsersController < ApplicationController
   get '/login' do
     erb :'/users/login_user'
   end
+
+  get '/users/:id' do
+    @user = User.find_by(id: params[:id])
+    erb :'/users/show_user'
+  end
+
+  get '/users/:id/edit' do
+    @user = User.find_by(id: params[:id])
+    erb :'/users/edit_user'
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 3.times do |i|
-  @user = User.create(username: "user#{i+1}", email: "user#{i}@user.com", password: "test")
+  @user = User.create(username: "user#{i+1}", email: "user#{i+1}@user.com", password: "test")
   @artist = @user.artists.create(name: "artist#{i+1}", user_id: i+1)
   3.times do |j|
     Concert.create(ticket_price: 15.00, location: "location#{j+1}", concert_date: "Jan #{j+1}, 2017", artist_id: i+1)
