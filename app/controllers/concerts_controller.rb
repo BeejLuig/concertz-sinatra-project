79c464412957fb6e777205e417487ad6de96912b
BeejLuig <bjcantlupe@gmail.com>
2017-01-02 23:03:38 -0500

    add new concert route

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -8,6 +8,19 @@ class ConcertsController < ApplicationController
     end
   end
 
+  post '/concerts' do
+    @concert = Concert.create(artist: Artist.find_or_create_by(name: params[:artist][:name], user_id: current_user.id),
+                              location: params[:location],
+                              concert_date: Date.parse(params[:concert_date]),
+                              description: params[:description],
+                              ticket_price: params[:ticket_price])
+    if @concert.valid?
+      redirect to "/concerts/#{@concert.id}"
+    else
+      redirect to '/concerts/new'
+    end
+  end
+
   get '/concerts/:id' do
     @concert = Concert.find_by(id: params[:id])
     erb :'/concerts/show_concert'
